9afa9c5420b50a336ba9b7295cbdb91d8ca06fa5
Sylvain Decout <sdecout@xebia.fr>
2019-12-14 16:44:08 +0100

    Solution for exercise 6

--- src/test/java/fr/xebia/jqwik/exercise6/ContractTest.java
+++ src/test/java/fr/xebia/jqwik/exercise6/ContractTest.java
@@ -1,10 +1,12 @@
 package fr.xebia.jqwik.exercise6;
 
-import net.jqwik.api.Assume;
-import net.jqwik.api.Disabled;
-import net.jqwik.api.ForAll;
-import net.jqwik.api.Property;
+import net.jqwik.api.*;
+import net.jqwik.api.Tuple.Tuple2;
 
+import java.time.Instant;
+
+import static net.jqwik.api.Arbitraries.*;
+import static net.jqwik.api.Combinators.combine;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatNullPointerException;
 
@@ -37,7 +39,6 @@ class ContractTest {
         assertThat(contract.covers(incident)).isFalse();
     }
 
-    @Disabled("Don't forget to remove this annotation!")
     /**
      * TODO: Fix test so that it succeeds.
      * <hr/>
@@ -45,12 +46,55 @@ class ContractTest {
      * <p>Hint #2: Group "contract" and "incident" arguments into a consistent "Tuple". Cf. <a href="https://jqwik.net/docs/current/user-guide.html#flat-mapping-with-tuple-types">Flat Mapping with Tuple Types</a></p>
      */
     @Property
-    void should_cover_incident_matching_coverage(@ForAll Contract contract, @ForAll Incident incident) {
+    void should_cover_incident_matching_coverage(@ForAll("contractAndIncidentWithSameCountry") Tuple2<Contract, Incident> contractAndIncident) {
+        final var contract = contractAndIncident.get1();
+        final var incident = contractAndIncident.get2();
+
         Assume.that(contract.validitySpan().contains(incident.date()));
         Assume.that(contract.coveredVehicleTypes().contains(incident.vehicle().type()));
-        Assume.that(contract.coveredCountries().contains(incident.country()));
 
         assertThat(contract.covers(incident)).isTrue();
+    }
+
+    @Provide
+    Arbitrary<Tuple2<Contract, Incident>> contractAndIncidentWithSameCountry() {
+        return defaultFor(Country.class)
+                .flatMap(country -> incidentArbitraryWithCountry(country)
+                        .flatMap(incident -> contractArbitraryWithCountry(country)
+                                .map(contract -> Tuple.of(contract, incident))));
+    }
+
+    private static Arbitrary<Incident> incidentArbitraryWithCountry(final Country country) {
+        return combine(
+                strings(),
+                of(Incident.Type.class),
+                defaultFor(Instant.class),
+                defaultFor(Vehicle.class)
+        ).as((id, type, date, vehicle) ->
+                Incident.builder()
+                        .id(id)
+                        .type(type)
+                        .date(date)
+                        .country(country)
+                        .vehicle(vehicle)
+                        .build()
+        );
+    }
+
+    private static Arbitrary<Contract> contractArbitraryWithCountry(final Country country) {
+        return combine(
+                defaultFor(PolicyNumber.class),
+                defaultFor(ValiditySpan.class),
+                defaultFor(VehicleType.class).set().ofMaxSize(20),
+                defaultFor(Country.class).set().filter(list -> list.contains(country))
+        ).as((policyNumber, validitySpan, vehicleTypes, countries) ->
+                Contract.builder()
+                        .policyNumber(policyNumber)
+                        .validitySpan(validitySpan)
+                        .coveredCountries(countries)
+                        .coveredVehicleTypes(vehicleTypes)
+                        .build()
+        );
     }
 
 }
