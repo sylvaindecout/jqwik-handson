0f0016fc03ad8c5a3cd215dccdabc03c8e4b9cdb
Sylvain Decout <sdecout@xebia.fr>
2019-12-21 17:44:05 +0100

    Fix instructions for exercise 4

--- src/test/java/fr/xebia/jqwik/exercise4/AlertRoutingServiceTest.java
+++ src/test/java/fr/xebia/jqwik/exercise4/AlertRoutingServiceTest.java
@@ -39,7 +39,7 @@ class AlertRoutingServiceTest {
     }
 
     /**
-     * TODO: Fix test so that it does not rely on an example for country.
+     * TODO: Add country as an argument of the test method, so that the test does not rely on an example.
      * <br/>
      * <p>Hint #1: <a href="https://jqwik.net/docs/current/user-guide.html#simple-arbitrary-providers">Simple arbitrary providers</a></p>
      * <p>Hint #2: <a href="https://jqwik.net/docs/current/user-guide.html#assumptions">Assumptions</a></p>
